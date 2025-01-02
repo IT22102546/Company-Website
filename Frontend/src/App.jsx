5ebb683b79e32a4c0ef8899cd49ab26e86af5908
Sanjana Nimesh <sanjana.nim2001@gmail.com>
2025-01-03 00:03:56 +0530

    Frontend created with tailwind css

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './Components/Header'
+import Home from './Pages/Home'
+import Footer from './Components/Footer'
+
+
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+
+        <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>    
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  )
+}
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,29 @@
+import flowbitePlugin from 'flowbite/plugin';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        tangerine: ['Tangerine'],
+        cinzel: ['Cinzel'],
+        Lavish: ['sans-serif'],
+      },
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+    },
+  },
+  plugins: [flowbitePlugin],
+};
